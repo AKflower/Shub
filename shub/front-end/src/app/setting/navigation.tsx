79808a574c28ea1d34c2293ef989dcbbd02acbeb
AKflower <anhkhoabqv@gmail.com>
2023-11-14 16:26:05 +0700

    SettingPage

---
+++ shub/front-end/src/app/setting/navigation.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+const Search = () => { 
+
+    return (
+        <nav>
+            <div className="">
+                <ul className="">
+                    <li><Link href=""><a href="" className="">Profile Setting</a></Link></li>
+                    <li><Link href=""><a href="" className=""></a></Link></li>
+                    <li><Link href=""><a href="" className=""></a></Link></li>
+                    <li><Link href=""><a href="" className=""></a></Link></li>
+                    <li><Link href=""><a href="" className=""></a></Link></li>
+                    
+                </ul>
+            </div>
+        </nav>
+    )
+}
---
+++ shub/front-end/src/app/setting/page.tsx
@@ -0,0 +1,5 @@
+export default function SettingPage() {
+    return (
+        <div className="">Hahahah</div>
+    )
+}
