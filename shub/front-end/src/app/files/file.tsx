a066ecfd78f7e561354fb8b702630ca5e8f3fb2c
dev_enterprise <tranquocdungb4@gmail.com>
2023-12-03 23:59:35 +0700

    update something strong

--- shub/front-end/src/app/files/file.tsx
+++ shub/front-end/src/app/files/file.tsx
@@ -7,28 +7,24 @@ import { useShub } from '../Provider/Provider';
 import axios from 'axios';
 import { usePathname, useRouter } from 'next/navigation'; 
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-<<<<<<< HEAD
+
 import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
-=======
+
 import Breadcrumbs from '../components/breadcumbs/breadcrumbs';
->>>>>>> 85a0db823cdd74ae4acf9c406bd043a81af71b76
+
 
 export default function File () {
-    const router = useRouter();
-    const { showFile, change, resetSelect, hideOption } = useShub();
-    const [show, setShow] = useState('');
+    const {  change, resetSelect, hideOption } = useShub();
     const [blank, setBlank] = useState('');
     const [folder, setFolder] = useState([]);
     const [file, setFile] = useState([]);
     const pathname = usePathname()
     
     useEffect(() => {
-        if (pathname != '/files') {
-            setShow('show')
-        } else setShow('')
+        
 
         const fetchData = async () => {
             const token = localStorage.getItem('token');
@@ -69,12 +65,10 @@ export default function File () {
    
       
     return (
-<<<<<<< HEAD
+
         <div className={cx('container')}>
-            {show &&(<button onClick={() => {
-                router.push(pathname.slice(0, pathname.lastIndexOf('/')))
-              
-            }}><ArrowBackIosNewIcon /></button>)}
+            <Breadcrumbs />
+            
             
             {folder[0] && <FolderSection folders={folder}/>}
             {file[0] && <FileSection files={file}/>}
@@ -85,16 +79,8 @@ export default function File () {
                 <div className={cx('message')}>It feels lonely here...</div>
                 
             </div>)}
-=======
-        <div className={styles.container}>
-            <Breadcrumbs />
-            {/* {show &&(<button onClick={() => {
-                router.push(pathname.slice(0, pathname.lastIndexOf('/')))
-              
-            }}><ArrowBackIosNewIcon /></button>)} */}
-            <FolderSection folders={folder}/>
-            <FileSection files={showFile}/>
->>>>>>> 85a0db823cdd74ae4acf9c406bd043a81af71b76
+
+            
         </div>
     )
 }
